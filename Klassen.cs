bb536fb0e8eae1d03bc40bc50608f5cd1f36c188
Quarky25 <152741302+Quarky25@users.noreply.github.com>
2023-12-10 19:53:14 +0100

    Add files via upload

---
+++ Klassen.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using static Calculator.Program;
+
+
+
+namespace CalculatorKlassen
+{
+    internal class Menu
+    {
+        public static void PrintMenu(bool clear = true)
+        {
+            if (clear)
+                Console.Clear();
+
+            // Displaying Calculator Menu
+            Console.WriteLine("Calculator Menu");
+            Console.WriteLine("1: Addition");
+            Console.WriteLine("2: Subtraction");
+            Console.WriteLine("3: Multiplacation");
+            Console.WriteLine("4: Division");
+            Console.WriteLine("5: Exit");
+
+
+          
+
+        }
+    }
+
+    class Operation
+    {
+        public static void Division()
+        {
+            var firstNumber = ReadNumber("Please enter your first number");
+            var secondNumber = ReadNumberExludingZero("Please enter your second number");
+
+            double result = firstNumber * 1.0 / secondNumber;
+            Console.WriteLine(result);
+        }
+
+        public static void Multiplacation()
+        {
+            var firstNumber = ReadNumber("Please enter your first number");
+            var secondNumber = ReadNumber("Please enter your second number");
+
+            double result = firstNumber * secondNumber;
+            Console.WriteLine(result);
+        }
+
+        public static void Subtraction()
+        {
+            var firstNumber = ReadNumber("Please enter your first number");
+            var secondNumber = ReadNumber("Please enter your second number");
+
+            double result = firstNumber - secondNumber;
+            Console.WriteLine(result);
+        }
+
+        public static void Addition()
+        {
+            var firstNumber = ReadNumber("Please enter your first number");
+            var secondNumber = ReadNumber("Please enter your second number");
+
+            double result = firstNumber + secondNumber;
+            Console.WriteLine(result);
+        }
+
+
+
+
+    }
+
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,127 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CalculatorKlassen.Operation;
+using static CalculatorKlassen.Menu;
+
+
+
+
+namespace Calculator
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool isDone = false;
+            
+            
+            PrintMenu (false);
+            { 
+                var Result = ReadNumber("Please enter your choice:");
+
+                
+                switch (Result)
+                {
+                    case 1:
+                        Addition();
+                        break;
+
+                    case 2:
+                        Subtraction();
+                        break;
+
+                    case 3:
+                        Multiplacation();
+                        break;
+
+                    case 4:
+                        Division();
+                        break;
+
+                    case 5:
+                        isDone = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please enter a number between 1-5");
+                        break;
+                        
+                }
+                
+
+            }
+            while (!isDone);
+           
+
+        }
+
+
+       
+        public static int ReadNumber(string prompt)
+        {
+            bool isValid;
+            int Result;
+            
+
+            do
+            {
+                Console.WriteLine(prompt);
+                string userInput = Console.ReadLine()!;
+                isValid = int.TryParse(userInput, out Result);
+
+
+                if (!isValid)
+                {
+                    //Display for the stupid
+                    Console.WriteLine("Please enter a fucking Number, you Dipshit!.");
+                    Console.ReadKey(true);
+                    continue;
+                    //Do stuff while it is not valid
+                }  //Negated is valid
+
+
+            }
+            while (!isValid);
+
+            return Result;
+
+
+        }
+
+
+        public static int ReadNumberExludingZero(string prompt)
+        {
+            int number;
+            
+            do
+            {
+
+                number = ReadNumber(prompt);
+
+
+                if (number == 0)
+                {
+                    Console.WriteLine("Cannot divide by zero");
+                    Console.ReadKey(true);
+
+                }
+            } 
+            while (number == 0);
+
+            return number;
+        }
+
+       
+
+               
+             
+            
+      
+       
+            
+    }   
+}
