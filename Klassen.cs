87a0357f4c9a04c758644826a9a40824bca77369
Quarky25 <152741302+Quarky25@users.noreply.github.com>
2023-12-10 21:03:28 +0100

    Update Klassen.cs

--- Klassen.cs
+++ Klassen.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -12,11 +12,13 @@ namespace CalculatorKlassen
 {
     internal class Menu
     {
+        
         public static void PrintMenu(bool clear = true)
         {
+            //Clearing the console
             if (clear)
                 Console.Clear();
-
+            
             // Displaying Calculator Menu
             Console.WriteLine("Calculator Menu");
             Console.WriteLine("1: Addition");
@@ -31,10 +33,13 @@ namespace CalculatorKlassen
         }
     }
 
-    class Operation
+    class Operation : Functions
     {
+        
         public static void Division()
         {
+            
+            //Defining Division
             var firstNumber = ReadNumber("Please enter your first number");
             var secondNumber = ReadNumberExludingZero("Please enter your second number");
 
@@ -44,6 +49,7 @@ namespace CalculatorKlassen
 
         public static void Multiplacation()
         {
+            //Defining Multiplacation
             var firstNumber = ReadNumber("Please enter your first number");
             var secondNumber = ReadNumber("Please enter your second number");
 
@@ -53,6 +59,7 @@ namespace CalculatorKlassen
 
         public static void Subtraction()
         {
+            //Defining Subtraction
             var firstNumber = ReadNumber("Please enter your first number");
             var secondNumber = ReadNumber("Please enter your second number");
 
@@ -62,6 +69,7 @@ namespace CalculatorKlassen
 
         public static void Addition()
         {
+            //Defining Addition
             var firstNumber = ReadNumber("Please enter your first number");
             var secondNumber = ReadNumber("Please enter your second number");
 
@@ -74,6 +82,52 @@ namespace CalculatorKlassen
 
     }
 
+    class Functions
+    {
+        public static int ReadNumber(string prompt)
+        {
+            //IsValid is a bool that is true or false
+            bool isValid;
+            int result;
+            do
+            {
+                //Prompting the user to enter a number
+                Console.WriteLine(prompt);
+                string input = Console.ReadLine()!;
+                isValid = int.TryParse(input, out result);
+                if (!isValid)
+                {
+                    //If the user enters a letter, the program will display an error message and prompt the user to enter a number
+                    Console.WriteLine("Invalid input. Please enter a number");
+                }
+            } while (!isValid);
+
+            return result;
+        }
+
+        public static int ReadNumberExludingZero(string prompt)
+        {
+            //IsValid is a bool that is true or false
+            bool isValid;
+            int result;
+            do
+            {
+                // Prompting the user to enter a number other than zero                
+                Console.WriteLine(prompt);
+                string input = Console.ReadLine()!;
+                isValid = int.TryParse(input, out result);
+                if (!isValid || result == 0)
+                {
+                    //If the user enters zero, the program will display an error message and prompt the user to enter a number other than zero
+                    Console.WriteLine("Invalid input. Please enter a number that is not Zero");
+                }
+            } while (!isValid || result == 0);
+
+            return result;
+        }
+    }   
 }
+
+
 
 
